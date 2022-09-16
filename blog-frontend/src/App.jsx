6c8378a3c03da6ddcd054d448b3758b9fdf2cde4
zjdtm <syh05247@naver.com>
2022-09-16 17:28:12 +0900

    lazy-loading

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,34 +1,33 @@
-import React from "react";
+import React, { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import PostListPage from "./pages/PostListPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import PostPage from "./pages/PostPage";
-import Home from "./pages/Home";
-import PostWrite from "./pages/PostWrite";
-import ProductListPage from "./pages/ProductListPage";
-import CartPage from "./pages/CartPage";
-import Payment from "./payment/Payment";
-import ProductRegisterPage from "./pages/ProductRegisterPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
-// import MyPage from "./pages/MyPage";
+const PostListPage = lazy(() => import("./pages/PostListPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const PostPage = lazy(() => import("./pages/PostPage"));
+const Home = lazy(() => import("./pages/Home"));
+const PostWrite = lazy(() => import("./pages/PostWrite"));
+const ProductListPage = lazy(() => import("./pages/ProductListPage"));
+const CartPage = lazy(() => import("./pages/CartPage"));
+const Payment = lazy(() => import("./payment/Payment"));
+const ProductRegisterPage = lazy(() => import("./pages/ProductRegisterPage"));
+const ProductDetailPage = lazy(() => import("./pages/ProductDetailPage"));
+
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/posts" element={<PostListPage />} />
-        <Route path="/posts/@:username/:postId" element={<PostPage />} />
-        <Route path="/posts/write" element={<PostWrite />} />
-        <Route path="/products" element={<ProductListPage />} />
-        <Route path="/products/:productId" element={<ProductDetailPage />} />
-        <Route path="/product/register" element={<ProductRegisterPage />} />
-        <Route path="/cart" element={<CartPage />} />
+        <Route path="/" element={<React.Suspense fallback={<>loading...</>}><Home /></React.Suspense>} />
+        <Route path="/login" element={<React.Suspense fallback={<>loading...</>}><LoginPage /></React.Suspense>} />
+        <Route path="/register" element={<React.Suspense fallback={<>loading...</>}><RegisterPage /></React.Suspense>} />
+        <Route path="/posts" element={<React.Suspense fallback={<>loading...</>}><PostListPage /></React.Suspense>} />
+        <Route path="/posts/@:username/:postId" element={<React.Suspense fallback={<>loading...</>}><PostPage /></React.Suspense>} />
+        <Route path="/posts/write" element={<React.Suspense fallback={<>loading...</>}><PostWrite /></React.Suspense>} />
+        <Route path="/products" element={<React.Suspense fallback={<>loading...</>}><ProductListPage /></React.Suspense>} />
+        <Route path="/products/:productId" element={<React.Suspense fallback={<>loading...</>}><ProductDetailPage /></React.Suspense>} />
+        <Route path="/product/register" element={<React.Suspense fallback={<>loading...</>}><ProductRegisterPage /></React.Suspense>} />
+        <Route path="/cart" element={<React.Suspense fallback={<>loading...</>}><CartPage /></React.Suspense>} />
         <Route path="/payment" element={<Payment />} />
-        {/* <Route path="/myPage" element={<MyPage />} /> */}
       </Routes>
     </>
   );
